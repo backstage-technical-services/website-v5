111b03c8890a345328b23de4fe598115af37c7e2
Ben Jones <ben@bnjns.uk>
2024-02-11 18:00:09 +0000

    chore(api): remove invalid auth policy tests

--- api/src/test/kotlin/org/backstage/auth/PolicyTests.kt
+++ api/src/test/kotlin/org/backstage/auth/PolicyTests.kt
@@ -19,8 +19,7 @@ import org.backstage.AuthHelpers
 import org.junit.jupiter.api.Test
 import java.util.*
 
-private val USER_CORRECT = UUID.fromString("566e8ad1-f5e3-4062-93e4-541c961f0c98")
-private val USER_INCORRECT = UUID.fromString("891832e6-ee0b-4085-870b-0cbba0a62671")
+private val USER_ID = UUID.fromString("566e8ad1-f5e3-4062-93e4-541c961f0c98")
 
 @QuarkusTest
 class PolicyInjectionTests {
@@ -35,7 +34,7 @@ class PolicyInjectionTests {
 
 class SingletonPolicyTests : FunSpec() {
     init {
-        val user = AuthHelpers.createMockedIdentity(userId = USER_CORRECT.toString())
+        val user = AuthHelpers.createMockedIdentity(userId = USER_ID.toString())
 
         context("a singleton policy and an entity") {
             val policy = object : Policy<ExampleEntity>(user) {
@@ -44,16 +43,9 @@ class SingletonPolicyTests : FunSpec() {
                     "deny" -> deny()
                     "lambda-valid" -> authorise { true }
                     "lambda-invalid" -> authorise { false }
-//                    "no-entity" -> authorise(entity) { true }
-//                    "entity-lambda-valid" -> authorise(entity) { true }
-//                    "entity-lambda-invalid" -> authorise(entity) { false }
-//                    "author-interface" -> authorise(entity) { e -> user.isAuthor(e) }
-//                    "author-lambda-valid" -> authorise(entity) { e -> user.isAuthor(e) { USER_CORRECT } }
-//                    "author-lambda-invalid" -> authorise(entity) { e -> user.isAuthor(e) { USER_INCORRECT } }
                     else -> throw IllegalArgumentException("Unknown action '$action'")
                 }
             }
-            val entity = ExampleEntity(createdBy = USER_CORRECT)
 
             test("using an action that allows should not throw an exception") {
                 shouldNotThrow<UnauthorizedException> {
@@ -73,41 +65,6 @@ class SingletonPolicyTests : FunSpec() {
             test("using an action that uses an unsuccessful lambda should throw an exception") {
                 shouldThrow<UnauthorizedException> {
                     policy.authorise("lambda-invalid")
-                }
-            }
-            test("using using a lambda that requires an entity but no entity passed should throw an exception") {
-                shouldThrow<UnauthorizedException> {
-                    policy.authorise("no-entity", null)
-                }
-            }
-            test("the entity passes the lambda should not throw an exception") {
-                shouldNotThrow<UnauthorizedException> {
-                    policy.authorise("entity-lambda-valid", entity)
-                }
-            }
-            test("the entity does not pass the lambda should throw an exception") {
-                shouldThrow<UnauthorizedException> {
-                    policy.authorise("entity-lambda-invalid", entity)
-                }
-            }
-            test("the hasAuthor lambda is successful should not throw an exception") {
-                shouldNotThrow<UnauthorizedException> {
-                    policy.authorise("author-lambda-valid", entity)
-                }
-            }
-            test("the hasAuthor lambda is unsuccessful should throw an exception") {
-                shouldThrow<UnauthorizedException> {
-                    policy.authorise("author-lambda-invalid", entity)
-                }
-            }
-            test("implementing hasAuthor and the entity has the correct author should not throw an exception") {
-                shouldNotThrow<UnauthorizedException> {
-                    policy.authorise("author-interface", entity)
-                }
-            }
-            test("implementing hasAuthor and the entity does not have the correct author should throw an exception") {
-                shouldThrow<UnauthorizedException> {
-                    policy.authorise("author-interface", ExampleEntity(createdBy = USER_INCORRECT))
                 }
             }
         }
