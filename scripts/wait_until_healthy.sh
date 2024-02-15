f7245f40cb721a76be0d5aa8b3c32670153724b9
Ben Jones <ben@bnjns.uk>
2024-02-15 15:23:02 +0000

    ci: wait for api to become healthy before running e2e tests

--- scripts/wait_until_healthy.sh
+++ scripts/wait_until_healthy.sh
@@ -1,24 +1,18 @@
 #!/usr/bin/env sh
-docker compose up \
-  --detach \
-  --build \
-  --force-recreate \
-  api
+readonly containerName="$1"
 
 # Check every 1s for 30s that the container becomes healthy
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
 do
-    health=$(docker inspect --format='{{.State.Health.Status}}' backstage-api)
+    health=$(docker inspect --format='{{.State.Health.Status}}' "$containerName")
     echo "Health = $health"
 
     if [ "${health}" = "healthy" ]; then
-        docker compose down &> /dev/null
         exit 0
     fi
 
     sleep 1
 done
 
-echo "Container was unhealthy"
-docker compose down &> /dev/null
+echo "Container did not become healthy within 30s"
 exit 1
