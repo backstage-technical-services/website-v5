a6f61704d72698f0b2ca244b13522992a4458aa8
Ben Jones <ben@bnjns.uk>
2024-02-15 16:53:55 +0000

    ci: configure sonarcloud job with coverage

--- ui/cypress/support/e2e.ts
+++ ui/cypress/support/e2e.ts
@@ -2,5 +2,8 @@
 import { registerCommands } from '@quasar/quasar-app-extension-testing-e2e-cypress'
 registerCommands()
 
+// Code coverage
+import '@cypress/code-coverage/support.js'
+
 // Import custom commands
 import './commands'
